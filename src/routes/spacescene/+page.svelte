079721c7df9d3333bba25e56215c7b1e4237f044
Herbert-Moreno <joseherbet1@gmail.com>
2025-09-04 20:33:18 -0300

    voltar para github actions

--- src/routes/spacescene/+page.svelte
+++ src/routes/spacescene/+page.svelte
@@ -2,8 +2,6 @@
     import SpatialWindow from "../../components/SpatialWindow.svelte";
     import SpaceScene from "../../pages/spacescene.svelte";
     import FloatingWindow from "../../components/FloatingWindow.svelte";
-    export const prerender = true;
-
 </script>
 
 <div class="flex flex-1 flex-nowrap">
