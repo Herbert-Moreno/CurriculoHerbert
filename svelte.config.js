be839070fbe25e357c2500a5ccdcec18846906d7
Herbert-Moreno <joseherbet1@gmail.com>
2025-09-04 11:39:26 -0300

    concluido deploy CI/CD

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+const repoName = "CurriculoHerbert";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -11,10 +14,10 @@ const config = {
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html'
+      fallback: 'index.html' // 🔑 SPA mode
     }),
     paths: {
-      base: process.env.GITHUB_PAGES ? "/Curriculo" : ""
+      base: dev ? "" : `/${repoName}`, // 🔑
     }
   }
 };
