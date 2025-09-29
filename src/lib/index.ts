fb80ab02dcc091cc4e601e5863085d8a6b723e30
ClockWritesDev <clockwritestwitter@gmail.com>
2025-09-28 22:43:19 -0300

    rework pfp e translation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 import { writable } from "svelte/store";
+import { browser } from "$app/environment";
 
 export let global_page = writable("landing");
 
@@ -8,7 +9,11 @@ import US_Json from '../lang/en_us.json';
 import ES_Json from '../lang/es_es.json';
 import PT_Json from '../lang/pt_br.json';
 
-let page_locale = navigator.language;
+let page_locale = "en-US";
+
+if (browser) {
+    page_locale = navigator.language;
+}
 export let text_localized = {};
 
 switch (page_locale.split("-")[0]) {
