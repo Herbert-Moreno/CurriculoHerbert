397a87d2b04ad16d4953fdad4131b53266994597
ClockWritesDev <clockwritestwitter@gmail.com>
2026-08-31 12:09:05 -0300

    reworked

--- src/components/ComputerModel.svelte
+++ src/components/ComputerModel.svelte
@@ -7,8 +7,8 @@
   let laptop_screen_texture = $derived(projectTexture);
   
   const loadingPromise = Promise.all([
-    useGltf(`/Computer.glb`),
-    useTexture(`/ComputerTexture.png`)
+    useGltf(`${import.meta.env.BASE_URL}/Computer.glb`),
+    useTexture(`${import.meta.env.BASE_URL}/ComputerTexture.png`)
   ]);
 </script>
 
